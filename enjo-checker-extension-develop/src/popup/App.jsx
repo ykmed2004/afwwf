74efffb2fd808993237f543994290a27ed7cf703
ykmed <ykmed2005@gmail.com>
2025-09-14 07:38:18 +0900

    first commit

---
+++ enjo-checker-extension-develop/src/popup/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+        <h1>炎上チェッカー🔥🔎</h1>
+        <p>
+            Twitter/Xの投稿ボタンが「炎上チェック」ボタンに変わります。
+        </p>
+        </div>
+    );
+}
+
+export default App;
---
+++ enjo-checker-extension-develop/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'popup.html',
+        background: 'src/scripts/background.js',
+        content: 'src/scripts/content.js',
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `chunks/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+})
